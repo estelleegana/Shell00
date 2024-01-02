b10681e07814e1224a98e7ba003ccf350168fe47
goldishmthfckz <124591796+goldishmthfckz@users.noreply.github.com>
2024-01-02 16:59:19 +0100

    Update ft_printhexa.c

--- srcs/ft_printhexa.c
+++ srcs/ft_printhexa.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../incl/ft_printf.h"
 
 int	ft_nblen(unsigned int nb)
 {
